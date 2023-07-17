c2dbb1818221c0bb52652cfe940cbdd06d522240
Montu-Gohain <leomontugohain@gmail.com>
2023-07-17 11:07:21 +0530

    Added an external packaged called colored via pip

--- first.py
+++ first.py
@@ -1,10 +1,15 @@
 from colored import fg
-color = fg('yellow')
+
+color = fg("yellow")
 myname = "Montu Gohain"
 current_year = 2023
 lang = "Python"
 
-sentence = "Hello I am %s, in the year %d, I will study %s in depth" % (myname, current_year, lang)
+sentence = "Hello I am %s, in the year %d, I will study %s in depth" % (
+    myname,
+    current_year,
+    lang,
+)
 print(sentence)
 
 # ^ Variables
@@ -15,10 +20,12 @@ line3 = "*               Welcome to Python Programming              *"
 
 sequence = [line1, line2, line3, line2, line1]
 
-def PrintWelcome() :
+
+def PrintWelcome():
     # ? Looping over the elements of list sequence and printing them one by one
 
-    for line in sequence : 
-       print(color + line)
+    for line in sequence:
+        print(color + line)
+
 
 PrintWelcome()
