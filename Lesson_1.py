9de4802bb7e17d269ea9a1410cf0a9833cb582fa
Montu-Gohain <leomontugohain@gmail.com>
2023-07-17 22:50:43 +0530

    Covering Topics lesson wise

---
+++ Lesson_1.py
@@ -0,0 +1,11 @@
+# Todo :  We'll Learn how to use variables and operators.
+username = "Harry Potter"  # ? Here = is called the assignment operator.
+print("Currently the user is : %s" % username)
+# If I want to change the value of username to something else.
+username = "Montu Gohain"
+print("Currently the user is : %s" % username)
+num1 = 10
+num2 = 3
+sum_res = num1 + num2
+
+print("Total added value is : %d" % sum_res)
