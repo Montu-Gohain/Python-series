bd5adaf688d093af311e060ed5b6c3dffe17b39c
Montu-Gohain <leomontugohain@gmail.com>
2023-07-17 10:51:12 +0530

    Learned how to use for loop and create a function in python

--- first.py
+++ first.py
@@ -1,6 +1,24 @@
+from colored import fg
+color = fg('yellow')
 myname = "Montu Gohain"
 current_year = 2023
 lang = "Python"
 
 sentence = "Hello I am %s, in the year %d, I will study %s in depth" % (myname, current_year, lang)
 print(sentence)
+
+# ^ Variables
+
+line1 = "************************************************************"
+line2 = "*                                                          *"
+line3 = "*               Welcome to Python Programming              *"
+
+sequence = [line1, line2, line3, line2, line1]
+
+def PrintWelcome() :
+    # ? Looping over the elements of list sequence and printing them one by one
+
+    for line in sequence : 
+       print(color + line)
+
+PrintWelcome()
